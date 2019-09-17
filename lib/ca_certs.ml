902c973410eac01e197bff70ee8f3b72445e23ad
Etienne Millon <me@emillon.org>
2019-09-17 15:03:28 +0200

    Remove commented list

--- lib/ca_certs.ml
+++ lib/ca_certs.ml
@@ -1,12 +1,3 @@
-(*
-    "/etc/ssl/certs/ca-certificates.crt" (* Debian/Ubuntu/Gentoo etc.*);
-    (* "/etc/pki/tls/certs/ca-bundle.crt",                  // Fedora/RHEL 6*)
-    (* "/etc/ssl/ca-bundle.pem",                            // OpenSUSE*)
-    (* "/etc/pki/tls/cacert.pem",                           // OpenELEC*)
-      "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem" (* CentOS/RHEL 7*);
-      (* "/etc/ssl/cert.pem",                                 // Alpine Linux*)
-   *)
-
 let rec detect_list =
   let open Lwt in
   function
