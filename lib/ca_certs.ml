c1a33813f7b44f0de7df6273180cc2f40594d667
Hannes Mehnert <hannes@mehnert.org>
2020-10-02 16:33:43 +0200

    add support for Darwin #3 and https://stackoverflow.com/questions/32472337/osx-export-system-certificates-from-keychain-in-pem-format-programmatically

--- lib/ca_certs.ml
+++ lib/ca_certs.ml
@@ -1,10 +1,15 @@
+let detect_one path =
+  let path = Fpath.v path in
+  match Bos.OS.Path.exists path with
+  | Ok true -> Ok path
+  | _ -> Error (`Msg "not found")
+
 let rec detect_list = function
   | [] -> Error (`Msg "no trust anchor file exists")
   | path :: paths -> (
-      let path = Fpath.v path in
-      match Bos.OS.Path.exists path with
-      | Ok true -> Ok path
-      | _ -> detect_list paths )
+      match detect_one path with
+      | Ok path -> Ok path
+      | Error _ -> detect_list paths )
 
 (* from https://golang.org/src/crypto/x509/root_linux.go *)
 let linux_locations =
@@ -18,22 +23,32 @@ let linux_locations =
   ]
 
 (* from https://golang.org/src/crypto/x509/root_bsd.go *)
-let openbsd_locations = [ (* OpenBSD *) "/etc/ssl/cert.pem" ]
+let openbsd_location = "/etc/ssl/cert.pem"
 
-let freebsd_locations =
-  [ (* FreeBSD *) "/usr/local/share/certs/ca-root-nss.crt" ]
+let freebsd_location = "/usr/local/share/certs/ca-root-nss.crt"
+
+let macos_keychain_location =
+  "/System/Library/Keychains/SystemRootCertificates.keychain"
 
 let ta_file_raw () =
   let open Rresult.R.Infix in
   if Sys.win32 then Error (`Msg "windows is not supported at the moment")
   else
     let cmd = Bos.Cmd.(v "uname" % "-s") in
-    (Bos.OS.Cmd.(run_out cmd |> out_string |> success) >>= function
-     | "FreeBSD" -> Ok freebsd_locations
-     | "OpenBSD" -> Ok openbsd_locations
-     | "Linux" -> Ok linux_locations
-     | s -> Error (`Msg ("unknown system " ^ s)))
-    >>= fun locs -> detect_list locs
+    Bos.OS.Cmd.(run_out cmd |> out_string |> success) >>= function
+    | "FreeBSD" -> detect_one freebsd_location
+    | "OpenBSD" -> detect_one openbsd_location
+    | "Linux" -> detect_list linux_locations
+    | "Darwin" ->
+        let cmd =
+          Bos.Cmd.(
+            v "security" % "find-certificate" % "-a" % "-p"
+            % macos_keychain_location)
+        in
+        let tmpfile = Fpath.v (Filename.temp_file "cacert" "pem") in
+        Bos.OS.Cmd.(run_out cmd |> out_file tmpfile |> success) >>| fun () ->
+        tmpfile
+    | s -> Error (`Msg ("unknown system " ^ s))
 
 let trust_anchor_filename () =
   let open Rresult.R.Infix in
