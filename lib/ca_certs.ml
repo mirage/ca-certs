007f023c3c062baa2853c4aa1d3f4db670be5c45
Hannes Mehnert <hannes@mehnert.org>
2020-10-02 15:15:12 +0200

    add OpenSUSE trust anchor file

--- lib/ca_certs.ml
+++ lib/ca_certs.ml
@@ -6,10 +6,15 @@ let rec detect_list =
       Lwt_unix.file_exists path >>= fun exists ->
       if exists then return_some (`Ca_file path) else detect_list paths
 
+(* from https://golang.org/src/crypto/x509/root_linux.go *)
 let locations =
   [
+    (* Debian/Ubuntu/Gentoo etc. *)
     "/etc/ssl/certs/ca-certificates.crt";
+    (* CentOS/RHEL 7 *)
     "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem";
+    (* OpenSUSE *)
+    "/etc/ssl/ca-bundle.pem";
   ]
 
 let detect () = detect_list locations
