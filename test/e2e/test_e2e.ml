4e1ca05ac8f3d11371eddea398b57bc19450a302
Hannes Mehnert <hannes@mehnert.org>
2020-10-02 14:58:51 +0200

    fix build (test dependencies) adapt to newer x509 API (0.11.0+) comment out revoked certificate

--- test/e2e/test_e2e.ml
+++ test/e2e/test_e2e.ml
@@ -4,32 +4,28 @@ type result =
   | Authentication_failure of X509.Validation.validation_error
 
 let pp_leaf_validation_error ppf = function
-  | `LeafCertificateExpired _ -> Format.fprintf ppf "expired"
-  | `LeafInvalidName _ -> Format.fprintf ppf "invalid name"
-  | `LeafInvalidVersion _ -> Format.fprintf ppf "invalid version"
-  | `LeafInvalidExtensions _ -> Format.fprintf ppf "invalid extensions"
+  | `LeafCertificateExpired _ -> Format.fprintf ppf "leaf: expired"
+  | `LeafInvalidName _ -> Format.fprintf ppf "leaf: invalid name"
+  | `LeafInvalidVersion _ -> Format.fprintf ppf "leaf: invalid version"
+  | `LeafInvalidExtensions _ -> Format.fprintf ppf "leaf: invalid extensions"
+  | e -> X509.Validation.pp_validation_error ppf e
 
 let pp_result ppf = function
   | Accepted -> Format.pp_print_string ppf "Accepted"
   | Unknown_exception e ->
       Format.fprintf ppf "Unknown_exception: %s" (Printexc.to_string e)
-  | Authentication_failure (`Leaf e) ->
-      Format.fprintf ppf "Authentication failure (leaf: %a)"
-        pp_leaf_validation_error e
   | Authentication_failure e ->
-      Format.fprintf ppf "Authentication failure: %a"
-        X509.Validation.pp_validation_error e
-
-let to_authenticator_param = function
-  | Some x -> x
-  | None ->
-      print_endline "defaulting to null auth";
-      `No_authentication_I'M_STUPID
+      Format.fprintf ppf "Authentication failure (%a)"
+        pp_leaf_validation_error e
 
 let make_client () =
-  let open Lwt in
-  Ca_certs.detect () >>= fun r ->
-  to_authenticator_param r |> X509_lwt.authenticator >|= fun authenticator ->
+  let open Lwt.Infix in
+  Ca_certs.detect () >>= (function
+      | Some r -> X509_lwt.authenticator r
+      | None ->
+        print_endline "no ca certificates found";
+        let verify ~host:_ _ = Error `InvalidChain in
+        Lwt.return verify) >|= fun authenticator ->
   Tls.Config.client ~authenticator ()
 
 let connect client host =
@@ -57,7 +53,7 @@ let main () =
       "self-signed.badssl.com";
       "expired.badssl.com";
       "untrusted-root.badssl.com";
-      "revoked.badssl.com";
+      (* "revoked.badssl.com"; *)
       "extended-validation.badssl.com";
     ]
 
